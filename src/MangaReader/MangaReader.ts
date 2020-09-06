7f631c4fcbd4fa96ccab31dc5060b4be81fb20fa
Synstress <lommie.h@hotmail.com>
2020-09-06 18:38:51 +0200

    implemented getChapterDetails

--- src/MangaReader/MangaReader.ts
+++ src/MangaReader/MangaReader.ts
@@ -18,10 +18,10 @@ export class MangaReader extends Source {
 
   getMangaDetailsRequest(ids: string[]): Request[] {
     return [createRequestObject({
-        metadata: ids[0],
+      metadata: ids[0],
       url: "https://mangareader.net/" + ids[0],
       method: 'GET',
-      
+
     })]
   }
   getMangaDetails(data: any, metadata: any): Manga[] {
@@ -30,24 +30,24 @@ export class MangaReader extends Source {
 
 
     return [createManga({
-        id: metadata,
-        titles: [$("#main .d40").text().replace(/\s?(manga)\s?$/gi, '')],
-        image: $('#main .d38 img').attr('src')!.toString(),
-        rating: 0,
-        status: status == "ongoing"? MangaStatus.ONGOING : MangaStatus.COMPLETED,
-        artist: $("#main table.d41 tr:nth-child(6) td:nth-child(2)").text(),
-        author: $("#main table.d41 tr:nth-child(5) td:nth-child(2)").text(),
-        desc: $("#main .d46 p").text(),
-        hentai: false
-      })]
-  } 
+      id: metadata,
+      titles: [$("#main .d40").text().replace(/\s?(manga)\s?$/gi, '')],
+      image: $('#main .d38 img').attr('src')!.toString(),
+      rating: 0,
+      status: status == "ongoing" ? MangaStatus.ONGOING : MangaStatus.COMPLETED,
+      artist: $("#main table.d41 tr:nth-child(6) td:nth-child(2)").text(),
+      author: $("#main table.d41 tr:nth-child(5) td:nth-child(2)").text(),
+      desc: $("#main .d46 p").text(),
+      hentai: false
+    })]
+  }
 
   getChaptersRequest(mangaId: string): Request {
     return createRequestObject({
-        metadata: mangaId,
+      metadata: mangaId,
       url: "https://mangareader.net/" + mangaId,
       method: 'GET',
-      
+
     })
   }
   getChapters(data: any, metadata: any): Chapter[] {
@@ -57,62 +57,76 @@ export class MangaReader extends Source {
     let nameRegex = /^(.+)(?=\s\d)/gi
     let chapterNumberRegex = /(\d+$)/gi
     for (let chapter of chapters) {
-        let chapterTitleAnchor = $('a', chapter)
-        let chapterNumber = chapterTitleAnchor.text().match(chapterNumberRegex)
-        // paste stuff here from now on
-        chapterList.push(createChapter({
-            id: chapterNumber != null ? chapterNumber[0].toString() : "0",
-            chapNum: Number(chapterNumber != null ? chapterNumber[0].toString() : "0"),
-            langCode: LanguageCode.ENGLISH,
-            volume: 0,
-            mangaId: metadata,
-            name: "",
-            time: new Date($("td:nth-child(2)", chapter).text()),
-          }))
+      let chapterTitleAnchor = $('a', chapter)
+      let chapterNumber = chapterTitleAnchor.text().match(chapterNumberRegex)
+      // paste stuff here from now on
+      chapterList.push(createChapter({
+        id: chapterNumber != null ? chapterNumber[0].toString() : "0",
+        chapNum: Number(chapterNumber != null ? chapterNumber[0].toString() : "0"),
+        langCode: LanguageCode.ENGLISH,
+        volume: 0,
+        mangaId: metadata,
+        name: "",
+        time: new Date($("td:nth-child(2)", chapter).text()),
+      }))
     }
     return chapterList
-}
+  }
 
-searchRequest(query: SearchRequest, page: number): Request | null {
+  searchRequest(query: SearchRequest, page: number): Request | null {
     return createRequestObject({
-        url: "https://www.mangareader.net/search/?nsearch=&msearch="+query.title,
-        method: "GET"
-      })
+      url: "https://www.mangareader.net/search/?nsearch=&msearch=" + encodeURI(query.title ?? ""),
+      method: "GET"
+    })
   }
   search(data: any, metadata: any): MangaTile[] | null {
     let $ = this.cheerio.load(data);
     let searchResults = $("#ares table tr").toArray()
 
-let mangas = []
-for (let result of searchResults) {
-  mangas.push(createMangaTile({
-    id: $("a", result).attr('href')!.toString().replace(/\//gi, ''),
-    image: $("div[data-src]", result).attr('data-src')!.toString(),
-    title: createIconText({text: $("a", result).text()})
-  }))
-}
+    let mangas = []
+    for (let result of searchResults) {
+      mangas.push(createMangaTile({
+        id: $("a", result).attr('href')!.toString().replace(/\//gi, ''),
+        image: $("div[data-src]", result).attr('data-src')!.toString().replace(/\/\//gi, 'https://'),
+        title: createIconText({ text: $("a", result).text() })
+      }))
+    }
 
-return mangas
+    return mangas
   }
 
   getChapterDetailsRequest(mangaId: string, chapId: string): Request {
     return createRequestObject({
-      url: `${MD_CHAPTER_API}/${chapId}`,
+      url: "https://mangareader.net/" + mangaId + "/" + chapId,
       method: 'GET',
-      incognito: true
+      cookies: [createCookie({
+        name: "drs",
+        value: "2",
+        domain: "https://mangareader.net"
+      })] ,
+      metadata: {
+        chapId: chapId,
+        mangaId: mangaId
+      } 
     })
   }
 
   getChapterDetails(data: any, metadata: any): ChapterDetails {
-    let chapterDetails = JSON.parse(data) as any
+    let $ = this.cheerio.load(data);
+    let allPages = $("#main .d56 .mI img").toArray()
+    let pages = []
+    for (let page of allPages) {
+      pages.push($(page).attr('src')!.toString().replace(/(?<=\=['"])\/\//g, 'https://'))
+    }
 
     return createChapterDetails({
-      id: chapterDetails['id'].toString(),
-      longStrip: parseInt(chapterDetails['long_strip']) == 1,
-      mangaId: chapterDetails['manga_id'].toString(),
-      pages: chapterDetails['page_array'].map((x: string) => `${chapterDetails['server']}${chapterDetails['hash']}/${x}`)
+      id: metadata.chapId,
+      longStrip: false,
+      mangaId: metadata.mangaId,
+      pages: pages
     })
   }
+  }
 
-}
+
 
